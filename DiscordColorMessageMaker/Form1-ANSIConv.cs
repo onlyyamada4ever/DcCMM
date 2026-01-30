7697b0989c84c5e86e891e668b04db8ecd55f80c
Elif_918 <onlyyamada4ever@gmail.com>
2026-01-30 12:18:23 +0400

    Create Form1-ANSIConv.cs

---
+++ DiscordColorMessageMaker/Form1-ANSIConv.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DiscordColorMessageMaker
+{
+    public partial class Form1 : Form
+    {
