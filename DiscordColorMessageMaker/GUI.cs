00e5df95ce60459ea4fe53daaa1ebcaf0943e557
Elif_918 <onlyyamada4ever@gmail.com>
2026-01-30 12:14:45 +0400

    Update GUI.cs

--- DiscordColorMessageMaker/GUI.cs
+++ DiscordColorMessageMaker/GUI.cs
@@ -1,1 +1,15 @@
-e
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DiscordColorMessageMaker
+{
+    public partial class Form1 : Form
+    {
+    
+    }
+}
